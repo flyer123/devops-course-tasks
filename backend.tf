469d595e56cb58f148da3de6a31459eeeca0ada5
flyer123 <shirinskiy.yuriy@gmail.com>
2024-11-19 20:45:11 +0200

    remove VPC

--- backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "shju-terraform-state-bucket-2024-11"
-    key            = "terraform/state"
-    region         = "eu-north-1"
-    dynamodb_table = "terraform-lock-table"
-  }
-}
