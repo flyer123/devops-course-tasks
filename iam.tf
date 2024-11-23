49e3eb11fc9c84e96179434e01af64a5c62b40c9
flyer123 <shirinskiy.yuriy@gmail.com>
2024-11-23 12:17:02 +0200

    yaml changed to apply role fix

--- iam.tf
+++ iam.tf
@@ -8,3 +8,25 @@ resource "aws_iam_openid_connect_provider" "openidconnect" {
 
   thumbprint_list = ["ffffffffffffffffffffffffffffffffffffffff"]
 }
+data "aws_iam_policy_document" "oidc" {
+  statement {
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+
+    principals {
+      type        = "Federated"
+      identifiers = [aws_iam_openid_connect_provider.openidconnect.arn]
+    }
+
+    condition {
+      test     = "StringEquals"
+      values   = ["sts.amazonaws.com"]
+      variable = "token.actions.githubusercontent.com:aud"
+    }
+
+    condition {
+      test     = "StringEquals"
+      values   = ["repo:flyer123/devops-course-tasks:ref:refs:/heads/main*"]
+      variable = "token.actions.githubusercontent.com:sub"
+    }
+  }
+}
