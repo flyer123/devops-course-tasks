75cd6e51b882735b3b505f8d2fb7d14afcf49994
flyer123 <shirinskiy.yuriy@gmail.com>
2024-11-23 11:35:08 +0200

    add policy

--- roles.tf
+++ roles.tf
@@ -8,3 +8,26 @@ resource "aws_iam_openid_connect_provider" "openidconnect" {
 
   thumbprint_list = ["d89e3bd43d5d909b47a18977aa9d5ce36cee184c"]
 }
+
+data "aws_iam_policy_document" "oidc" {
+  statement {
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+
+    principals {
+      type        = "Federated"
+      identifiers = [aws_iam_openid_connect_provider.this.arn]
+    }
+
+    condition {
+      test     = "StringEquals"
+      values   = ["sts.amazonaws.com"]
+      variable = "token.actions.githubusercontent.com:aud"
+    }
+
+    condition {
+      test     = "StringLike"
+      values   = ["repo:flyer123/devops-course-tasks/*"]
+      variable = "token.actions.githubusercontent.com:sub"
+    }
+  }
+}
