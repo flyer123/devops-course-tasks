70d41d18c761483b1d13897ad6427502844971f7
flyer123 <shirinskiy.yuriy@gmail.com>
2024-11-23 13:23:21 +0200

    add

--- iam.tf
+++ iam.tf
@@ -8,3 +8,25 @@ resource "aws_iam_openid_connect_provider" "openidconnect" {
 
   thumbprint_list = ["ffffffffffffffffffffffffffffffffffffffff"]
 }
+data "aws_iam_policy_document" "oidc" {
+  statement {
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+
+    principals {
+      type        = "Federated"
+      identifiers = [aws_iam_openid_connect_provider.this.arn]
+    }
+
+    condition {
+      test     = "StringEquals"
+      values   = ["sts.amazonaws.com"]
+      variable = "token.actions.githubusercontent.com:aud"
+    }
+
+    condition {
+      test     = "StringLike"
+      values   = ["repo:flyer123/*"]
+      variable = "token.actions.githubusercontent.com:sub"
+    }
+  }
+}
