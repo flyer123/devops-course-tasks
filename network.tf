8e10a41df1df9b77155df9c6445a9638b4fc8fad
flyer123 <shirinskiy.yuriy@gmail.com>
2024-12-16 20:38:53 +0200

    add subnets, plan

---
+++ network.tf
@@ -0,0 +1,42 @@
+data "aws_availability_zones" "available-azs" {
+  state = "available"
+}
+
+resource "aws_vpc" "vpc-tf" {
+  cidr_block = var.cidr_block_vpc
+  tags {
+    Name = "terraform-vpc"
+  }
+}
+
+resource "aws_subnet" "public-subnets-tf_1" {
+    depends_on = [aws_vpc.vpc-tf]
+    vpc_id = aws_vpc.vpc-tf.id
+    cidr_block = "10.20.0.0/24"
+    tolist(data.aws_availability_zones.available-azs.names)[0]
+   
+}
+
+resource "aws_subnet" "public-subnets-tf_2" {
+    depends_on = [aws_vpc.vpc-tf]
+    vpc_id = aws_vpc.vpc-tf.id
+    cidr_block = "10.20.1.0/24"
+    tolist(data.aws_availability_zones.available-azs.names)[1]
+}
+
+resource "aws_subnet" "private-subnets-tf_1" {
+    depends_on = [aws_vpc.vpc-tf]
+    vpc_id = aws_vpc.vpc-tf.id
+    cidr_block = "10.20.2.0/24"
+    tolist(data.aws_availability_zones.available-azs.names)[0]
+    
+}
+
+resource "aws_subnet" "private-subnets-tf_2" {
+    depends_on = [aws_vpc.vpc-tf]
+    vpc_id = aws_vpc.vpc-tf.id
+    cidr_block = "10.20.3.0/24"
+    tolist(data.aws_availability_zones.available-azs.names)[1]
+    
+}
+
--- variables.tf
+++ variables.tf
@@ -1,3 +1,7 @@
 variable "github_role" {
   default = "GithubActionsRole"
 }
+
+variable "vpc-cidr" {
+    default = "10.20.0.0/16"
+}
