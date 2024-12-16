e9e598ae261293a90d4515c09f940100fc8c79b1
flyer123 <shirinskiy.yuriy@gmail.com>
2024-12-16 21:35:45 +0200

    try add count

--- network.tf
+++ network.tf
@@ -4,36 +4,75 @@ data "aws_availability_zones" "available-azs" {
 
 resource "aws_vpc" "vpc-tf" {
   cidr_block = var.cidr_block_vpc
+  tags = {
+    Name = "terraform-vpc"
+  }
 }
 
-resource "aws_subnet" "public-subnets-tf_1" {
+resource "aws_subnet" "public-subnets-tf" {
+  count = 2
   depends_on        = [aws_vpc.vpc-tf]
   vpc_id            = aws_vpc.vpc-tf.id
-  cidr_block        = "10.20.0.0/24"
-  availability_zone = tolist(data.aws_availability_zones.available-azs.names)[0]
-
+  cidr_block        = "10.20.${count.index}.0/24"
+  availability_zone = tolist(data.aws_availability_zones.available-azs.names)[count.index]
+  tags = {
+    Name = "public-subnet-${count.index}"
+  }
 }
 
-resource "aws_subnet" "public-subnets-tf_2" {
+resource "aws_subnet" "private-subnets-tf" {
+  count = 2
   depends_on        = [aws_vpc.vpc-tf]
   vpc_id            = aws_vpc.vpc-tf.id
-  cidr_block        = "10.20.1.0/24"
-  availability_zone = tolist(data.aws_availability_zones.available-azs.names)[1]
+  cidr_block        = "10.20.${count.index + 2}.0/24"
+  availability_zone = tolist(data.aws_availability_zones.available-azs.names)[count.index]
+  tags = {
+    Name = "private-subnet-${count.index}"
+  }
 }
 
-resource "aws_subnet" "private-subnets-tf_1" {
-  depends_on        = [aws_vpc.vpc-tf]
-  vpc_id            = aws_vpc.vpc-tf.id
-  cidr_block        = "10.20.2.0/24"
-  availability_zone = tolist(data.aws_availability_zones.available-azs.names)[0]
-
+/*
+# internet gateway
+resource "aws_internet_gateway" "internet-gateway" {
+  vpc_id = aws_vpc.vpc-tf.id
+  tags = {
+    Name = "terraform-igw"
+  }
 }
 
-resource "aws_subnet" "private-subnets-tf_2" {
-  depends_on        = [aws_vpc.vpc-tf]
-  vpc_id            = aws_vpc.vpc-tf.id
-  cidr_block        = "10.20.3.0/24"
-  availability_zone = tolist(data.aws_availability_zones.available-azs.names)[1]
+# route table for public subnets
+resource "aws_route_table" "public-rtb" {
+  vpc_id = aws_vpc.vpc-tf.id
+  depends_on = [aws_internet_gateway.internet-gateway]
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.internet-gateway.id
+  }
 
+  tags = {
+    Name = "terraform_public_rtb"
+    Tier = "public"
+  }
 }
 
+
+# route table for private subnets
+resource "aws_route_table" "private-rtb" {
+  vpc_id = aws_vpc.vpc-tf.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    # nat_instance here
+  }
+  tags = {
+    Name = "terraform-private-rtb"
+    Tier = "private"
+  }
+}
+
+# public subnet route table associations
+
+# private subnet route table associations
+
+# NAT-instance
+*/
