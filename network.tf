4548feadb19361f03aa7395ec060aeb5438f569f
flyer123 <shirinskiy.yuriy@gmail.com>
2024-12-16 20:40:40 +0200

    add subnets fix, plan

--- network.tf
+++ network.tf
@@ -13,7 +13,7 @@ resource "aws_subnet" "public-subnets-tf_1" {
     depends_on = [aws_vpc.vpc-tf]
     vpc_id = aws_vpc.vpc-tf.id
     cidr_block = "10.20.0.0/24"
-    tolist(data.aws_availability_zones.available-azs.names)[0]
+    availability_zone = tolist(data.aws_availability_zones.available-azs.names)[0]
    
 }
 
@@ -21,14 +21,14 @@ resource "aws_subnet" "public-subnets-tf_2" {
     depends_on = [aws_vpc.vpc-tf]
     vpc_id = aws_vpc.vpc-tf.id
     cidr_block = "10.20.1.0/24"
-    tolist(data.aws_availability_zones.available-azs.names)[1]
+    availability_zone = tolist(data.aws_availability_zones.available-azs.names)[1]
 }
 
 resource "aws_subnet" "private-subnets-tf_1" {
     depends_on = [aws_vpc.vpc-tf]
     vpc_id = aws_vpc.vpc-tf.id
     cidr_block = "10.20.2.0/24"
-    tolist(data.aws_availability_zones.available-azs.names)[0]
+    availability_zone = tolist(data.aws_availability_zones.available-azs.names)[0]
     
 }
 
@@ -36,7 +36,7 @@ resource "aws_subnet" "private-subnets-tf_2" {
     depends_on = [aws_vpc.vpc-tf]
     vpc_id = aws_vpc.vpc-tf.id
     cidr_block = "10.20.3.0/24"
-    tolist(data.aws_availability_zones.available-azs.names)[1]
+    availability_zone = tolist(data.aws_availability_zones.available-azs.names)[1]
     
 }
 
