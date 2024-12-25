668b40ed3f49badadbf038186a712fe5864b1325
flyer123 <flyer123@users.noreply.github.com>
2024-12-25 16:47:06 +0000

    Terraform fmt

--- network.tf
+++ network.tf
@@ -157,15 +157,15 @@ resource "aws_security_group" "nat_security_group" {
 
   egress = [
     {
-      description     = "Default egress"
-      from_port       = 0
-      to_port         = 0
-      protocol        = "-1"
-      cidr_blocks     = ["0.0.0.0/0"]
+      description      = "Default egress"
+      from_port        = 0
+      to_port          = 0
+      protocol         = "-1"
+      cidr_blocks      = ["0.0.0.0/0"]
       ipv6_cidr_blocks = []
-      prefix_list_ids = []
-      security_groups = []
-      self            = true
+      prefix_list_ids  = []
+      security_groups  = []
+      self             = true
     }
   ]
 }
