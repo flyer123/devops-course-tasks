40122ab217116a45326aac52948fb4b06ec1efd1
flyer123 <flyer123@users.noreply.github.com>
2024-12-27 10:43:47 +0000

    Terraform fmt

--- network.tf
+++ network.tf
@@ -61,7 +61,7 @@ resource "aws_route_table" "private-rtb" {
   vpc_id = aws_vpc.vpc-tf.id
 
   route {
-    cidr_block     = "0.0.0.0/0"
+    cidr_block  = "0.0.0.0/0"
     instance_id = aws_instance.nat_aws_instance.id
   }
   tags = {
