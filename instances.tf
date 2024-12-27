23ebd4f75c9dc8162368d47d9e3e6fcbfd755afb
flyer123 <flyer123@users.noreply.github.com>
2024-12-27 11:01:50 +0000

    Terraform fmt

--- instances.tf
+++ instances.tf
@@ -64,12 +64,12 @@ resource "aws_instance" "nat_testing_aws_instances" {
 # bastion host
 resource "aws_instance" "bastion_host_instance" {
   #depends_on                      = [aws_security_group.test_instance_sg]
-  ami                    = data.aws_ami.amzn_linux_2023_ami.id
+  ami                         = data.aws_ami.amzn_linux_2023_ami.id
   associate_public_ip_address = true
-  instance_type          = "t3.micro"
-  subnet_id              = aws_subnet.public-subnets-tf[0].id
-  vpc_security_group_ids = [aws_security_group.bastion_host_instance_sg.id]
-  key_name               = var.ec2_key_name
+  instance_type               = "t3.micro"
+  subnet_id                   = aws_subnet.public-subnets-tf[0].id
+  vpc_security_group_ids      = [aws_security_group.bastion_host_instance_sg.id]
+  key_name                    = var.ec2_key_name
 
   root_block_device {
     volume_size = "8"
