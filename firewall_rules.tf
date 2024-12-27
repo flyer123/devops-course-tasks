592d3455f10f3d122606479d932cb908c701841d
flyer123 <shirinskiy.yuriy@gmail.com>
2024-12-27 14:15:36 +0200

    egress bastion host rule

--- firewall_rules.tf
+++ firewall_rules.tf
@@ -96,3 +96,13 @@ resource "aws_security_group_rule" "bastion_host_instance_ssh_ingress" {
   cidr_blocks       = ["0.0.0.0/0"]
   security_group_id = aws_security_group.bastion_host_instance_sg.id
 }
+
+# bastion instance egress port 22
+resource "aws_security_group_rule" "bastion_host_instance_ssh_eggress" {
+  type              = "egress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.bastion_host_instance_sg.id
+}
