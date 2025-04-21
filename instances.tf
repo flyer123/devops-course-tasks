fff75ffd659281a2d19b28dbc3d81e92cff8eef8
flyer123 <shirinskiy.yuriy@gmail.com>
2025-04-21 18:09:48 +0300

    destroy

--- instances.tf
+++ instances.tf
@@ -1,4 +1,4 @@
-# NAT-instance ami
+/*# NAT-instance ami
 data "aws_ami" "amzn_linux_2023_ami" {
   most_recent = true
   owners      = ["amazon"]
@@ -119,7 +119,7 @@ resource "aws_ssm_parameter" "k3s_token" {
   name  = "k3s_token"
   value = "empty"
   type  = "String"
-  overwrite   = true
+  #overwrite   = true
 
   lifecycle {
     ignore_changes = [value]
@@ -250,4 +250,4 @@ resource "aws_iam_instance_profile" "k3s_node" {
   depends_on = [aws_ssm_parameter.k3s_token]
   name       = "get_parameters"
   role       = aws_iam_role.get_parameters.name
-}
+}*/
