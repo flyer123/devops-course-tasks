b9f2e2e99db566c1301e4142fb83e3a6a0993447
flyer123 <shirinskiy.yuriy@gmail.com>
2025-05-25 16:34:46 +0300

    artifact master ip added

--- output.tf
+++ output.tf
@@ -1,5 +1,5 @@
-/*# output of ip of k3s master
+# output of ip of k3s master
 output "master_private_ip" {
   depends_on = [aws_instance.master]
   value      = aws_instance.master.private_ip
-}*/
+}
