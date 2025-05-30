af5a0aad27df9d18200d7749a94a59b3153e97c6
flyer123 <shirinskiy.yuriy@gmail.com>
2025-05-30 19:17:06 +0300

    run infrastructure

--- output.tf
+++ output.tf
@@ -1,4 +1,4 @@
-/*# output of ip addre of k3s master
+# output of ip addre of k3s master
 output "master_private_ip" {
   depends_on = [aws_instance.master]
   value      = aws_instance.master.private_ip
@@ -8,4 +8,4 @@ output "master_private_ip" {
 output "bastion_public_ip" {
   depends_on = [aws_instance.bastion_host_instance]
   value      = aws_instance.bastion_host_instance.public_ip
-}*/
+}
